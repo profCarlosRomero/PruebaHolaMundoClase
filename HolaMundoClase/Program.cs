09ec4d07917500e8baeb3a02cf7f97c71703031a
profCarlosRomero <prof.c.romero@gmail.com>
2023-05-04 08:55:20 -0300

    Agrego mensaje de despedida

--- HolaMundoClase/Program.cs
+++ HolaMundoClase/Program.cs
@@ -7,6 +7,7 @@ namespace HolaMundoClase
         static void Main(string[] args)
         {
             Console.WriteLine("Hola mundo!!!");
+            Console.WriteLine("Chau mundo!!!");
         }
     }
 }
